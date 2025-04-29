e59e8021cd47c3676a2189d92d28553bca987076
GHTuke <tuukka.huovilainen@myy.haaga-helia.fi>
2025-04-29 13:30:49 +0300

    Added required fields for GHPages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import Customer from './Customer.tsx'
 import Training from './Training.tsx'
 import Error from './Error.tsx'
 import Calendar from './Calendar.tsx'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
